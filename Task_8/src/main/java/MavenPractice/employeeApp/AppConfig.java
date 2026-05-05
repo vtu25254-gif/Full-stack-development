42255417c1f77f767f480b46fcd1ed1a6efbe62c
vtu25254-gif <vtu25254@veltech.edu.in>
2026-05-05 22:04:02 +0530

    Delete Task_8 directory

--- Task_8/src/main/java/MavenPractice/employeeApp/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package MavenPractice.employeeApp;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "MavenPractice.employeeApp")
-public class AppConfig {
-}
